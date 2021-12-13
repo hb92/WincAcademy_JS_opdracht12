7de9cf0f33254be1537a7fca63e686a4670b9c53
hb92 <britt.hellemans@gmail.com>
2021-12-13 14:52:39 +0100

    Add files via upload

--- script12.js
+++ script12.js
@@ -17,3 +17,17 @@ for (i=0; i < colors.length; i++) {
 //Array Methods
 
 colors.forEach(element => console.log(element));
+
+/*
+1: - while loop heeft 6 regels
+    - for loop heeft 4 regels
+2: 3
+3: Ik begrijp dat dit voor ervaren coders eenvoudiger is, maar ik vind de for methode toch net iets overzichtelijiker omdat ik dan beter zie en begrijp wat er gebeurd
+
+//vraag 4: Volgens mij is dit nog steeds een iteratie vermits overheen alle waardes wordt geloopt*/
+
+var obj = {a: 1, b: 2, c: 3, d:4, e:5};
+
+for (const prop in obj) {
+  console.log(`obj.${prop} = ${obj[prop]}`);
+}
